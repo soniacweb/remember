156d8c9b502342d5a1b47bce34ecbf80f4048f92
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-10-13 15:36:49 +0100

    added task functionality

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ function addTask(e) {
   if (taskInput.value === '') {
     alert('Add a task!')
     console.log(taskInput.value)
+
   }
   //create list of items from scratch- li element
   const li = document.createElement('li')
@@ -29,6 +30,20 @@ function addTask(e) {
   li.className = 'collection-item' 
   //create text node and append to li- create text node= we want to pass in whatever's been passed into the input
   li.appendChild(document.createTextNode(taskInput.value))
+  //create new link element
+  const link = document.createElement('a')
+  // then add a class to that
+  link.className = 'delete-item secondary-content'
+  //add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>'
+  //append the link to the li 
+  li.appendChild(link)
+
+  //now we want to append the li to the ul
+  taskList.appendChild(li)
+
+  //clear input
+  taskInput.value = ''
 
   e.preventDefault()
 
